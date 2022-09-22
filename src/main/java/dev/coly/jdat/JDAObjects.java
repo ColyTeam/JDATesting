70719085096ca924bca2157f9bc2da742c4f7bef
Gregyyy <gregyyy@pm.me>
2022-09-22 14:02:14 +0200

    Fix things that broke because JDA

--- src/main/java/dev/coly/jdat/JDAObjects.java
+++ src/main/java/dev/coly/jdat/JDAObjects.java
@@ -4,18 +4,19 @@ import dev.coly.util.Callback;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.NewsChannel;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.NewsChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
 import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import net.dv8tion.jda.api.requests.restaction.MessageAction;
+import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import net.dv8tion.jda.internal.JDAImpl;
 import org.jetbrains.annotations.NotNull;
 import org.mockito.Mockito;
@@ -52,7 +53,7 @@ public class JDAObjects {
      * @param subcommandGroup   the subcommand group of the slash command.
      * @param options           a map with all options for the slash command a user would have inputted.
      * @param messageCallback   a callback to receive messages that would be sent back to the channel with.
-     *                          {@link MessageChannel#sendMessage(Message)} or
+     *                          {@link MessageChannel#sendMessage(CharSequence)} or
      *                          {@link MessageChannel#sendMessageEmbeds(Collection)} for example.
      * @return                  a mocked {@link SlashCommandInteractionEvent}.
      */
@@ -76,7 +77,7 @@ public class JDAObjects {
      * @param messageCallback   a callback to receive messages that would be sent back to the channel with.
      * @param deferReply        a callback that is called when a deferred reply is called. The boolean is true when
      *                          the message is ephemeral and false if not.
-     *                          {@link MessageChannel#sendMessage(Message)} or
+     *                          {@link MessageChannel#sendMessage(CharSequence)} or
      *                          {@link MessageChannel#sendMessageEmbeds(Collection)} for example.
      * @return                  a mocked {@link SlashCommandInteractionEvent}.
      */
@@ -112,8 +113,9 @@ public class JDAObjects {
 
         when(event.reply(anyString())).thenAnswer(invocation ->
                 getReplyCallbackAction(getMessage(invocation.getArgument(0), channel), messageCallback));
-        when(event.reply(any(Message.class))).thenAnswer(invocation ->
-                getReplyCallbackAction(invocation.getArgument(0), messageCallback));
+        when(event.reply(any(MessageCreateData.class))).thenAnswer(invocation ->
+                getReplyCallbackAction(getMessage(invocation.getArgument(0, MessageCreateData.class).getContent(),
+                        channel), messageCallback));
         when(event.replyEmbeds(anyList())).thenAnswer(invocation ->
                 getReplyCallbackAction(getMessage(null, invocation.getArgument(0), channel),
                         messageCallback));
@@ -202,7 +204,7 @@ public class JDAObjects {
      * @param name              the name of the channel.
      * @param id                the id of the channel.
      * @param messageCallback   the callback used for returning the {@link Message} when for example
-     *                          {@link MessageChannel#sendMessage(Message)} or other methods are called.
+     *                          {@link MessageChannel#sendMessage(CharSequence)} or other methods are called.
      * @return                  a mocked {@link MessageChannel}.
      */
     public static MessageChannel getMessageChannel(String name, long id, Callback<Message> messageCallback) {
@@ -213,11 +215,11 @@ public class JDAObjects {
         when(channel.getId()).thenAnswer(invocation -> String.valueOf(id));
 
         when(channel.sendMessage(any(CharSequence.class)))
-                .thenAnswer(invocation -> getMessageAction(messageCallback,
+                .thenAnswer(invocation -> getMessageCreateAction(messageCallback,
                         getMessage(invocation.getArgument(0), channel)));
 
-        when(channel.sendMessage(any(Message.class)))
-                .thenAnswer(invocation -> getMessageAction(messageCallback,
+        when(channel.sendMessage(any(MessageCreateData.class)))
+                .thenAnswer(invocation -> getMessageCreateAction(messageCallback,
                         invocation.getArgument(0)));
 
         when(channel.sendMessageEmbeds(any(MessageEmbed.class), any(MessageEmbed[].class)))
@@ -225,26 +227,26 @@ public class JDAObjects {
                     List<MessageEmbed> embeds = invocation.getArguments().length == 1 ? new ArrayList<>() :
                             Arrays.asList(invocation.getArgument(1));
                     embeds.add(invocation.getArgument(0));
-                    return getMessageAction(messageCallback, getMessage(null, embeds, channel));
+                    return getMessageCreateAction(messageCallback, getMessage(null, embeds, channel));
                 });
 
         when(channel.sendMessageEmbeds(anyList()))
-                .thenAnswer(invocation -> getMessageAction(messageCallback,
+                .thenAnswer(invocation -> getMessageCreateAction(messageCallback,
                         getMessage(null, invocation.getArgument(0), channel)));
 
         return channel;
     }
 
     /**
-     * Get a mocked {@link MessageAction}.
+     * Get a mocked {@link MessageCreateAction}.
      *
      * @param messageCallback   the message callback that well return the {@link Message} when
-     *                          {@link MessageAction#queue()} is executed.
+     *                          {@link MessageCreateAction#queue()} is executed.
      * @param message           the message that will be used by the {@link Callback}.
-     * @return                  a mocked {@link MessageAction}.
+     * @return                  a mocked {@link MessageCreateAction}.
      */
-    public static MessageAction getMessageAction(Callback<Message> messageCallback, Message message) {
-        MessageAction messageAction = mock(MessageAction.class);
+    public static MessageCreateAction getMessageCreateAction(Callback<Message> messageCallback, Message message) {
+        MessageCreateAction messageAction = mock(MessageCreateAction.class);
         Mockito.doAnswer(invocation -> {
             messageCallback.callback(message);
             return null;
