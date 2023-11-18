8774978a390a0c771de20fb7ab6025824c8013cf
zvyap <koidsusan82@gmail.com>
2023-11-19 05:33:36 +0800

    Added test to new MultipleCallback

--- src/test/java/dev/coly/util/TestCallback.java
+++ src/test/java/dev/coly/util/TestCallback.java
@@ -2,12 +2,16 @@ package dev.coly.util;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 public class TestCallback {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("callbackInstances")
     public void testCallback() {
         Callback<String> callback = Callback.single();
         methodCallback(callback);
@@ -18,7 +22,8 @@ public class TestCallback {
         }
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("callbackInstances")
     public void testCallbackTimeout() {
         Callback<String> callback = Callback.single();
         methodCallback(callback);
@@ -29,14 +34,51 @@ public class TestCallback {
         }
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("callbackInstances")
     public void testCallbackTimeoutFailed() {
         Assertions.assertThrows(InterruptedException.class,
                 () -> Callback.single().await(100L, TimeUnit.MILLISECONDS));
     }
 
-    private void methodCallback(Callback<String> callback) {
+    @Test
+    public void testMultipleCallback() {
+        Callback<String> callback = Callback.multiple();
+        methodCallback(callback);
+        try {
+            Assertions.assertEquals("test", callback.await());
+        } catch (InterruptedException e) {
+            Assertions.fail(e);
+        }
+
+        methodCallback(callback);
+        callback.callback("test2");
+        try {
+            Assertions.assertEquals("test", callback.await());
+            Assertions.assertEquals("test2", callback.await());
+        } catch (InterruptedException e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    public void testMultipleCallbackLimit() {
+        Callback<String> callback = Callback.multiple(2);
+        methodCallback(callback);
+        methodCallback(callback);
+        Assertions.assertThrowsExactly(IllegalStateException.class, ()-> methodCallback(callback));
+    }
+
+    private static void methodCallback(Callback<String> callback) {
         callback.callback("test");
+    }
+
+    private static Stream<Callback> callbackInstances() {
+        return Stream.of(
+                Callback.single(),
+                Callback.multiple(),
+                Callback.multiple(5)
+        );
     }
 
 }
